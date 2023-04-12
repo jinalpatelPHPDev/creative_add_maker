2cb601297e8885fb2f05084f1dd7addca61bf1b0
akshay <akshay@itidoltechnologies.com>
2023-04-12 15:55:39 +0530

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -243,9 +243,12 @@ DB_PASSWORD="' . $dbPassword . '"
     private function storeConfiguration($key, $value)
     {
         $path = base_path('.env');
+
 echo "<pre>";
-print_r($path, str_replace(
-                $key . '=' . env($key), $key . '=' . $value, file_get_contents($path)));
+print_r($key . '=' . env($key));
+
+print_r($value);
+print_r(file_get_contents($path));
 echo "</pre>";
 exit();
         if (file_exists($path)) {
