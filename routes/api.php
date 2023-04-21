255c3238ae50f9cbc78a54c97db933960fda16a0
Jalpa <116339789+jinalpatel8213@users.noreply.github.com>
2023-04-21 15:46:53 +0530

    code

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::namespace ('Api')->middleware(['throttle'])->group(function () {
-    Route::namespace ('Api')->middleware(['throttle'])->group(['prefix'=>'CF9DF'],function(){
+Route::namespace ('Api')->middleware(['throttle'])->group(function () {
 	Route::post('/login', 'AuthApi@login');
     Route::post('/registration', 'AuthApi@registration');
     Route::post('/google-registration', 'AuthApi@google_registration');
@@ -12,8 +11,7 @@ use Illuminate\Support\Facades\Route;
     Route::post('/forgot-password', 'AuthApi@forgot_password');
 });
 
-// Route::namespace ('Api')->middleware(['throttle'])->group(function(){
-    Route::namespace ('Api')->middleware(['throttle'])->group(['prefix'=>'CF9DF'],function(){
+Route::namespace ('Api')->middleware(['throttle'])->group(function(){
     Route::post('/change-password', 'AuthApi@change_password');
     // Route::post('/register-fcm', 'AuthApi@register_fcm');
     // Route::post('/logout', 'AuthApi@logout');
