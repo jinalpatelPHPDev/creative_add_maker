4f2e21e0b8a08f57d590b43e5ec026d76c7c2d40
Jalpa <116339789+jinalpatel8213@users.noreply.github.com>
2023-04-21 15:32:14 +0530

    Added api key

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::namespace ('Api')->middleware(['throttle'])->group(function () {
+// Route::namespace ('Api')->middleware(['throttle'])->group(function () {
+    Route::prefix(env('API_KEY'))->namespace ('Api')->middleware(['throttle'])->group(function(){
 	Route::post('/login', 'AuthApi@login');
     Route::post('/registration', 'AuthApi@registration');
     Route::post('/google-registration', 'AuthApi@google_registration');
@@ -11,7 +12,8 @@ Route::namespace ('Api')->middleware(['throttle'])->group(function () {
     Route::post('/forgot-password', 'AuthApi@forgot_password');
 });
 
-Route::namespace ('Api')->middleware(['throttle'])->group(function(){
+// Route::namespace ('Api')->middleware(['throttle'])->group(function(){
+    Route::prefix(env('API_KEY'))->namespace ('Api')->middleware(['throttle'])->group(function(){
     Route::post('/change-password', 'AuthApi@change_password');
     // Route::post('/register-fcm', 'AuthApi@register_fcm');
     // Route::post('/logout', 'AuthApi@logout');
