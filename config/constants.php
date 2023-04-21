d54bd6b9b07eb54d9a2b933a737133045f8ebf1b
Jalpa <116339789+jinalpatel8213@users.noreply.github.com>
2023-04-21 16:20:55 +0530

    Code

--- config/constants.php
+++ config/constants.php
@@ -5,6 +5,9 @@ return [
     'envanto' => [
         'envanto_username' => env('ENVATO_USERNAME'),
         'envanto_code' => env('ENVATO_CODE'),
-    ]
+    ],
+    'routes' => [
+        'prefix' => env('API_KEY'),    
+    ],
 ];
 ?>
