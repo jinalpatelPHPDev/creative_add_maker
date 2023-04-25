d79006bf2f9edbd0abdea40fc420710e55162e1e
jinalpatelPHPDev <jinalss@itidoltechnologies.com>
2023-04-25 15:56:07 +0530

    code

--- routes/api.php
+++ routes/api.php
@@ -3,16 +3,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-$routes = function() {
+Route::namespace ('Api')->middleware(['throttle'])->prefix(config('constants.routes.prefix'))->group(function(){
 
 	Route::post('/login', 'AuthApi@login');
     Route::post('/registration', 'AuthApi@registration');
     Route::post('/google-registration', 'AuthApi@google_registration');
     Route::post('/phone-login', 'AuthApi@phone_login');
     Route::post('/forgot-password', 'AuthApi@forgot_password');
-}
+});
 
-Route::namespace ('Api')->middleware(['throttle'])->prefix(config('constants.routes.prefix'))->group($routes);
+
 Route::namespace ('Api')->middleware(['throttle'])->prefix(config('constants.routes.prefix'))->group(function(){
     Route::post('/change-password', 'AuthApi@change_password');
     // Route::post('/register-fcm', 'AuthApi@register_fcm');
